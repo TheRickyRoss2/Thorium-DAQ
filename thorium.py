0b4eacbeac120269e09606e0d3638986be6c434a
Ric Rodriguez <rirrodri@ucsc.edu>
2018-02-07 09:05:34 -0800

    Main app start
    Signed-off-by: Ric Rodriguez <rirrodri@ucsc.edu>

--- thorium.py
+++ thorium.py
@@ -2,6 +2,8 @@ __author__ = "Ric Rodriguez"
 __email__ = "therickyross2@gmail.com"
 __project__ = "Thorium DAQ"
 
+import configparser
+
 if __name__ == "__main__":
     print("""Welcome to
     
@@ -17,3 +19,15 @@ $$$$$$$$/ $$ |____    ______    ______  $$/  __    __  _____  ____
     """)
     print("By: Ric Rodriguez")
     print("For support or bug report submission: Please email Ric at therickyross2@gmail.com")
+
+    with open("config.ini") as settings:
+        daq_settings = settings.read()
+    config = configparser.RawConfigParser(allow_no_value=True)
+    config.read_file(open("config.ini"))
+
+    for section in config.sections():
+        print("Section: {}".format(section))
+        for options in config.options(section):
+            print("x {}::{}::{}".format(options, config.get(section, options), str(type(options))))
+    print(config.get('caen', 'iset'))
+    print(config.getboolean('lecroy', 'enable_channel1'))
