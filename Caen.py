4c0f2094cc02cdc0d2544b8c4d56ab10e3f9d4a6
Ric Rodriguez <rirrodri@ucsc.edu>
2018-02-07 00:54:29 -0800

    Testing

--- Caen.py
+++ Caen.py
@@ -4,25 +4,74 @@ class Caen(object):
     """
     Class for CAEN HV Power supply
     """
+    test_mode = False
     def check_return_status(self, return_status=""):
         if "ok" not in return_status.lower():
             raise SystemError("Invalid command")
 
-    def __init__(self, ip_address):
+    def __init__(self, ip_address=""):
         """
-        Constructor for scope object
-        Resets scope
+        Constructor for power supply object
         :param ip_address: Ethernet address of scope
         """
+
         if ip_address:
             self.inst = ResourceManager().open_resource("TCPIP0::" + ip_address + "::inst0::INSTR")
+            print(self.inst.query("$BD:0,CMD:MON,PAR:BDNAME"))
+            print(self.inst.query("$BD:0,CMD:MON,PAR:BDFREL"))
+            print(self.inst.query("$BD:0,CMD:MON,PAR:BDSNUM"))
         else:
-            print("No ip specified. Running in demo mode")
-        print(self.inst.query("*IDN?;"))
-        self.inst.timeout = 10000
-        print(self.inst.query("ASET;*OPC?"))
+            print("No ip address specified; Running in test mode.")
+            self.test_mode = True
 
-    def setup_channel(self, channel, ):
+    def setup_channel(self, channel, compliance):
+        """
+        Configure specified channel for the caen supply
+        :param channel: Channel number [0-3]
+        :param compliance:  compliance in uA
+        :return: None
+        """
 
-x = Caen()
-Caen.check_return_status("ok:statis")
+        command_format = "$BD:0,CMD:SET,CH:{},PAR:{},VAL:{}"
+
+        if self.test_mode:
+            return "TEST MODE: " + command_format.format(channel, "ISET", str(float(compliance)))
+
+        response = self.inst.query(command_format.format(channel, "ISET", str(float(compliance))))
+        self.check_return_status(response)
+
+    def set_output(self, channel, voltage):
+        """
+        Sets the channel output to a specified voltage
+        :param channel: Channel number [0-3]
+        :param voltage: Voltage level in V
+        :return:
+        """
+
+        command_format = "$BD:0,CMD:SET,CH:{},PAR:{},VAL:{}"
+
+        if self.test_mode:
+            return "TEST MODE: " + command_format.format(channel, "VSET", str(float(voltage)))
+
+        response = self.inst.query(command_format.format(channel, "VSET", str(float(voltage))))
+        self.check_return_status(response)
+
+    def enable_output(self, channel, enable=True):
+        """
+        Enables output if true for specified channel
+        :param channel: Channel number [0-3]
+        :param enable: True to turn on
+        :return: None
+        """
+
+        command_format = "$BD:0,CMD:SET,CH:{},PAR:{}"
+
+        if self.test_mode:
+            return "TEST MODE: " + command_format.format(channel, "ON" if enable else "OFF")
+
+        response = self.inst.query(command_format.format(channel, "ON" if enable else "OFF"))
+        self.check_return_status(response)
+        return
+
+
+
--- test_caen.py
+++ test_caen.py
@@ -1,10 +1,22 @@
 from unittest import TestCase
-
-
+from caen import Caen
 
 class TestCaen(TestCase):
+    """
+    Class for testing caen power supply command formatting
+    """
+    caen_test_runner = Caen()
+
     def test_check_return_status(self):
-        self.fail()
+        self.caen_test_runner.check_return_status("ok")
 
     def test_setup_channel(self):
+        self.caen_test_runner.setup_channel(1, 19)
+        self.assertEquals()
+        self.fail()
+
+    def test_set_output(self):
+        self.fail()
+
+    def test_enable_output(self):
         self.fail()
